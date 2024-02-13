a31983f3dc1c4f8a567f5eca2c8e6cda28452cfd
unknown <161aashuxtha@gmail.com>
2024-02-13 16:22:56 +0545

    updates

--- conditions/admin.py
+++ conditions/admin.py
@@ -5,7 +5,7 @@ from .models import condition
 
 @admin.register(condition)
 class conditionsAdmin(ImportExportMixin,admin.ModelAdmin):
-    list_display = ('name','Fan_Speed','Dryer_Speed','Burner_Speed','Durations')
+    list_display = ('name','Fan_Speed','Dryer_Speed','Burner_Speed','Temperature','Durations')
     list_filter = ('name',)
     search_fields = ('name',)
     list_per_page = 15
--- conditions/models.py
+++ conditions/models.py
@@ -5,6 +5,7 @@ class condition(models.Model):
     Fan_Speed = models.FloatField(null=True, blank=True, help_text="Speed for Fan")
     Dryer_Speed = models.FloatField(null=True, blank=True, help_text="Speed for Dryer")
     Burner_Speed = models.FloatField(null=True, blank=True, help_text="Speed for Burner")
+    Temperature = models.IntegerField(null=True, blank=True, help_text="Temperature")
     Durations = models.IntegerField(null=True, blank=True, help_text="Duration to keep car in main firing zone ")
 
     def __str__(self):
