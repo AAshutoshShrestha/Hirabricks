a01e8f9cc03fb3530fe22a6ca5eeb126aeaeda28
unknown <161aashuxtha@gmail.com>
2024-02-14 07:38:56 +0545

    filters

--- example/templatetags/app_filters.py
+++ example/templatetags/app_filters.py
@@ -4,6 +4,10 @@ register = template.Library()
 
 @register.filter(name='convert_to_hours_and_minutes')
 def convert_to_hours_and_minutes(value):
-    hours = value // 60
-    minutes = value % 60
-    return f"{hours} hours, {minutes} minutes"
+    try:
+        value = int(value)  # Convert value to integer
+        hours = value // 60
+        minutes = value % 60
+        return f"{hours} hours {minutes} minutes"
+    except (TypeError, ValueError):
+        return "Invalid input"
