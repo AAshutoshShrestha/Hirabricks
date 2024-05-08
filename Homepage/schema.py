14ee6a2a5679606b2dc3b5e80ec61fef243008ca
AAshutoshShrestha <aashutosh_1999@outlook.com>
2024-05-08 12:43:43 +0545

    api url schema updates

--- Homepage/schema.py
+++ Homepage/schema.py
@@ -1,12 +1,31 @@
 import graphene
 from graphene_django import DjangoObjectType
 
-from .models import Company_info,TeamMember
+from .models import *
+
+
+class ContactMessageType(DjangoObjectType):
+    class Meta:
+        model = ContactMessage
+        fields = ("id", "name", "email", "message", "created_at")
+
+class SocialLinkType(DjangoObjectType):
+    class Meta:
+        model = SocialLink
+        fields = ("id", "name", "url", "icon")
+
 
 class CompanyinfoType(DjangoObjectType):
     class Meta:
         model = Company_info
         fields = ("id", "name","address","phone_number","email")
+
+class TeamMemberType(DjangoObjectType):
+    class Meta:
+        model = TeamMember
+        fields = ("id", "name","position","image_url")
+
+
 
 class Company_Info_Query(graphene.ObjectType):
 	all_Company_info = graphene.List(CompanyinfoType)
@@ -14,18 +33,27 @@ class Company_Info_Query(graphene.ObjectType):
 	def resolve_all_Company_info(root,info):
 		return Company_info.objects.all()
 
-schema = graphene.Schema(query=Company_Info_Query)
-
-
-class TeamMemberType(DjangoObjectType):
-    class Meta:
-        model = TeamMember
-        fields = ("id", "name","position","image_url")
-
 class TeamMember_Query(graphene.ObjectType):
 	all_Team_members = graphene.List(TeamMemberType)
 	
 	def resolve_all_Team_members(root,info):
 		return TeamMember.objects.all()
 
-schema = graphene.Schema(query=TeamMember_Query)
+class ContactMessage_Query(graphene.ObjectType):
+    all_contact_messages = graphene.List(ContactMessageType)
+    
+    def resolve_all_contact_messages(root, info):
+        return ContactMessage.objects.all()
+
+class SocialLink_Query(graphene.ObjectType):
+    all_social_links = graphene.List(SocialLinkType)
+    
+    def resolve_all_social_links(root, info):
+        return SocialLink.objects.all()
+
+
+class Query(Company_Info_Query,TeamMember_Query,ContactMessage_Query, SocialLink_Query):
+    pass
+
+schema = graphene.Schema(query=Query)
+
